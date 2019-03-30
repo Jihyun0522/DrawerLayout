c73b7e7c0c4aecd251d84658c7a9b914640603d9
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-03-30 19:41:14 +0900

    success

--- app/src/main/java/kr/hs/drawerlayout/MainActivity.java
+++ app/src/main/java/kr/hs/drawerlayout/MainActivity.java
@@ -1,29 +1,29 @@
 package kr.hs.drawerlayout;
 
+import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private View drawerView;
-    private TextView txtPrompt, txtPrompt2;
+
+    private ImageButton buttonCloseDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtPrompt = (TextView) findViewById(R.id.prompt);
-        txtPrompt2 = (TextView) findViewById(R.id.prompt2);
-
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerView = (View)findViewById(R.id.drawer);
+        drawerView = (View) findViewById(R.id.drawer_menu);
 
         Button buttonOpenDrawer = (Button) findViewById(R.id.opendrawer);
         buttonOpenDrawer.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button buttonCloseDrawer = (Button) findViewById(R.id.closedrawer);
+        buttonCloseDrawer = (ImageButton) findViewById(R.id.closedrawer);
         buttonCloseDrawer.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
@@ -41,46 +41,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        drawerLayout.setDrawerListener(myDrawerListener);
-        drawerView.setOnTouchListener(new View.OnTouchListener() {
-
-            public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
-                return true;
-            }
-        });
     }
-
-    DrawerLayout.DrawerListener myDrawerListener = new DrawerLayout.DrawerListener() {
-
-        public void onDrawerClosed(View drawerView) {
-        }
-        public void onDrawerOpened(View drawerView) {
-        }
-
-        public void onDrawerSlide(View drawerView, float slideOffset) {
-            txtPrompt.setText("onDrawerSlide: "
-                    + String.format("%.2f", slideOffset));
-        }
-
-        public void onDrawerStateChanged(int newState) {
-            String state;
-            switch (newState) {
-                case DrawerLayout.STATE_IDLE:
-                    state = "STATE_IDLE";
-                    break;
-                case DrawerLayout.STATE_DRAGGING:
-                    state = "STATE_DRAGGING";
-                    break;
-                case DrawerLayout.STATE_SETTLING:
-                    state = "STATE_SETTLING";
-                    break;
-                default:
-                    state = "unknown!";
-            }
-
-            txtPrompt2.setText(state);
-        }
-    };
 
 }
